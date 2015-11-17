5cf3ea5be32cc67fc54aa04ba632a820b71d0d03
pinkjupiter <kkjy196508@naver.com>
2015-11-17 23:25:23 +0900

    init

--- programs/2d2d/character.py
+++ programs/2d2d/character.py
@@ -4,10 +4,12 @@ from pico2d import *
 __author__ = 'user'
 
 from effect import Effect1
-
+from effect import Effect2
+from effect import Effect3
 
 class character:
-   global effect1
+   global effect1,effect2,effect3
+   global effects
    image = None
    hpbar = None
    hpbar2 = None
@@ -16,7 +18,10 @@ class character:
    gravity = -30
    jumpMove = 0
    LEFT_IDLE, RIGHT_IDLE, LEFT_RUN, RIGHT_RUN,LEFT_JUMP,RIGHT_JUMP,LEFT_ATTACK,RIGHT_ATTACK =7,6,5,4,3,2,1,0
+
    effect1 = Effect1()
+   effect2 = [Effect2() for i in range(3)]
+   effect3 = Effect3()
    attack_frame = 0
    global Attack
    Attack = False
@@ -34,7 +39,7 @@ class character:
 
 
    def handle_event(self, event):
-
+        global effects,effect1,effect3
         global Attack
 
         if(event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
@@ -54,7 +59,7 @@ class character:
                     self.jumpMove = -5
                     self.frameSize = 1
 
-         # 공격
+         # 공격1
         elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_a):
             if self.state in (self.RIGHT_IDLE,self.RIGHT_RUN):
                 self.state = self.RIGHT_ATTACK
@@ -62,16 +67,55 @@ class character:
                 self.frame = 0
                 Attack = True
                 self.attack_frame = 0
-                effect1.init(self.x+70, self.y-15,0)
+                count = 0
+                effect1.init(self.x,self.y,0)
+
+            if self.state in (self.LEFT_IDLE,self.LEFT_RUN):
+                self.state = self.LEFT_ATTACK
+                self.frameSize = 74
+       # 공격2
+        elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_z):
+            if self.state in (self.RIGHT_IDLE,self.RIGHT_RUN):
+                self.state = self.RIGHT_ATTACK
+                self.frameSize = 7
+                self.frame = 0
+                Attack = True
+                self.attack_frame = 0
+                count = 0
+                for effects in effect2:
+                    effects.init(self.x+70+count*150, self.y-15,0)
+                    count=count+1
+                    print("카운트 %d",count)
+
             if self.state in (self.LEFT_IDLE,self.LEFT_RUN):
                 self.state = self.LEFT_ATTACK
                 self.frameSize = 7
+
+        # 공격3
+        elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_x):
+            if self.state in (self.RIGHT_IDLE,self.RIGHT_RUN):
+                self.state = self.RIGHT_ATTACK
+                self.frameSize = 7
+                self.frame = 0
+                Attack = True
+                self.attack_frame = 0
+                effect3.init(self.x, self.y, 0)
+            if self.state in (self.LEFT_IDLE,self.LEFT_RUN):
+                self.state = self.LEFT_ATTACK
+                self.frameSize = 7
+
+
+
         elif(event.type, event.key) == (SDL_KEYUP, SDLK_LEFT):
             if self.state in (self.LEFT_RUN,):
                 self.state = self.LEFT_IDLE
                 self.frameSize = 2
         elif(event.type, event.key) == (SDL_KEYUP, SDLK_RIGHT):
             if self.state in (self.RIGHT_RUN,):
+                self.state = self.RIGHT_IDLE
+                self.frameSize = 2
+        elif(event.type, event.key) == (SDL_KEYUP, SDLK_z) :
+            if self.state in (self.RIGHT_ATTACK,):
                 self.state = self.RIGHT_IDLE
                 self.frameSize = 2
         elif(event.type, event.key) == (SDL_KEYUP, SDLK_a) :
@@ -85,10 +129,16 @@ class character:
 
    def get_bb(self):
        return self.x - 25, self.y - 50, self.x + 25, self.y + 30
+
    def update(self):
         global Attack
-        if(Attack == True):
-            effect1.update()
+        global effects,effect1,effect3
+
+        effect1.update()
+        effect3.update()
+        for effects in effect2:
+            if(Attack == True):
+                effects.update()
         if self.attack_frame > 15:
             Attack = False
         self.attack_frame = self.attack_frame+1
@@ -139,12 +189,17 @@ class character:
         character.hpbar2 = load_image("resource/character_hp_stroke.png")
    def draw(self):
         global Attack
+        global effects,effect1,effect3
         self.hpbar2.draw(250,550)
         self.image.clip_draw(self.frame * 150, self.state * 150, 150, 150, self.x, self.y)
         self.hpbar.clip_draw(0,0,250+50,40,250-100+25,550)
         # self.hpbar.draw(250,550)
         draw_rectangle(*self.get_bb())
-        if(self.attack_frame <=15):
-            if(Attack == True):
-                effect1.draw()
-
+        for effects in effect2:
+            if(self.attack_frame <=15):
+                if(Attack == True):
+                    effects.draw(self.x,self.y)
+        if(Attack == True):
+            if(self.attack_frame <=15):
+                effect1.draw(self.x,self.y)
+                effect3.draw(self.x,self.y)
--- programs/2d2d/effect.py
+++ programs/2d2d/effect.py
@@ -13,12 +13,13 @@ class Effect1:
         self.y = y
         self.frame = frameNum
    def update(self):
-        Effect1.image = load_image("resource/fireattack/areaWarning_%d.png"%self.frame)
+
         self.frame = (self.frame+1) % self.frameSize
    def get_bb(self):
        return  self.x - 40, self.y - 40, self.x + 70, self.y + 40
-   def draw(self):
-        self.image.draw(self.x,self.y )
+   def draw(self,x,y):
+        Effect1.image = load_image("resource/fireattack/areaWarning_%d.png"%self.frame)
+        self.image.draw(self.x,self.y)
         draw_rectangle(*self.get_bb())
 
 class Effect2:
@@ -31,10 +32,30 @@ class Effect2:
         self.y = y
         self.frame = frameNum
    def update(self):
-        Effect1.image = load_image("resource/fireattack/areaWarning_%d.png"%self.frame)
+
         self.frame = (self.frame+1) % self.frameSize
    def get_bb(self):
-       return  self.x - 40, self.y - 40, self.x + 70, self.y + 40
-   def draw(self):
-        self.image.draw(self.x,self.y )
+       return  self.x +50, 0, self.x + 160, 600
+   def draw(self,x,y):
+       Effect2.image = load_image("resource/attack2/62_%d.png"%self.frame)
+       self.image.draw(self.x+100,self.y+250)
+       draw_rectangle(*self.get_bb())
+
+class Effect3:
+   image = None
+   def __init__(self):
+        self.frameSize = 20
+        self.frame = 0
+   def init(self,x,y,frameNum):
+        self.x = x
+        self.y = y
+        self.frame = frameNum
+   def update(self):
+
+        self.frame = (self.frame+1) % self.frameSize
+   def get_bb(self):
+       return  self.x +50, 0, self.x + 160, 600
+   def draw(self,x,y):
+        Effect3.image = load_image("resource/attack3/areaWarning_%d.png"%self.frame)
+        self.image.draw(self.x+100,self.y+250)
         draw_rectangle(*self.get_bb())
